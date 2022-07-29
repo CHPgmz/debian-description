b19f30088a1f7cde63aed05be2d77ea2b725a413
Alfredo <chpgomezito@gmail.com>
2022-07-29 13:48:27 -0500

    Primer commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Distribución Debian</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+  <body>
+      <main id="main">
+          <h1 id="title">Distribución de Linux Debian</h1>
+          <p id="sub">"Estable y Robusto"</p>
+          <div id="img-div">
+            <!--<img id="image" src="https://conceptodefinicion.de/wp-content/uploads/2017/08/Debian2.jpg" alt="No se cargo la imagen">-->
+            <img id="image" src="files/debian.jpg" alt="No se encontro">
+            <figcaption id="img-caption" >
+            Debian GNU/Linux es un sistema operativo gratuito desarrollado por miles de voluntarios de todo el mundo que colaboran a través de Internet.
+          </figcaption>
+          </div>
+          <section id="tribute-info">
+            <h2>¿Qué es Debian?</h2>
+            <p>El proyecto Debian fue fundado en el año 1993 por Ian Murdock, después de haber estudiado en la Universidad de Purdue. Él escribió el manifiesto de Debian, que utilizó como base para la creación de la distribución Linux Debian. Dentro de este texto, los puntos destacables son: mantener la distribución de manera abierta, coherente al espíritu del núcleo Linux y de GNU. </p>
+            <p>Aproximadamente cada año desde que se inició el programa se ha publicado una versión actualizada con nuevas características, colocando el ritmo de publicación desde el cambio de siglo en unos 24 meses. Además, para evitar confusiones, la primera versión oficial fue 1.1 y no 1.0 después de que, por error, un fabricante publicara una versión inédita llamada 1.0. Para designar dicha versión del software se le dio el nombre Buzz, uno de los personajes de la película animada Toy Story. El por qué es bastante simple: en ese momento, Bruce Perens, la persona que se hizo cargo de la línea del proyecto Debian, había trabajado como desarrollador de herramientas para el estudio Pixar (que es el estudio de cine creador de Toy Story).</p>
+            <h3 id="headline">Lanzamientos</h3>
+            <ul class="code-names">
+              <li>1.1 Buzz(17 de junio de 1996)</li>
+              <li>1.2 Rex (12 de diciembre de 1996)</li>
+              <li>1.3 Bo (2 de junio de 1997)</li>
+              <li>2.0 Hamm (24 de julio de 1998)</li>
+              <li>2.1 Slink (9 de marzo de 1999)</li>
+              <li>2.2 Potato (15 de agosto de 2000)</li>
+              <li>3.0 Woody (19 de julio de 2002)</li>
+              <li>3.1 Sarge (6 de junio de 2005)</li>
+              <li>4.0 Etch (8 de abril de 2007)</li>
+              <li>5.0 Lenny (14 de febrero de 2009)</li>
+              <li>6.0 Squeeze (6 de febrero de 2011)</li>
+              <li>7.0 Wheezy (4 de mayo de 2013)</li>
+              <li>8.0 Jessie (25 de abril de 2015)</li>
+              <li>9.0 Strech (17 de junio de 2017)</li>
+              <li>10 Buster (6 de julio de 2019)</li>
+              <li>11 Bullseye (14 de agosto de 2021)</li>
+            </ul>
+            <blockquote cite="https://www.debian.org/intro/philosophy.es.html">
+              <h2>Filosofía</h2>
+              <p>El proyecto Debian es una asociación de personas que comparten un objetivo: queremos crear un sistema operativo libre, disponible para todo el mundo. Ahora bien, cuando utilizamos el término «libre» no estamos hablando de dinero, sino que nos referimos a la libertad del software (N. del T.: en el original en inglés se utiliza el término «free», que es sinónimo de «gratuito» y de «libre»).</p>
+              <cite>Comunidad de Debian</cite>
+            </blockquote>
+            <h3>Para obtener mas information se puede obtener mas information en la siguiente url <a id="tribute-link" href="https://es.wikipedia.org/wiki/Debian_GNU/Linux" target="_blank">Wikipedia</a></h3>
+          </section>
+		</main>
+  </body>
+</html>
